be194518aeff9be348d661c8627ee7e52cd6dece
rickchung <rickjung88@gmail.com>
2016-01-24 15:06:24 +0800

    Add exercise3

---
+++ module1/problems/exercise3/answer.py
@@ -0,0 +1,9 @@
+from lib.support import get_dir_filenames, get_dict
+
+from datetime import datetime
+
+# Test Code
+filenames = get_dir_filenames('data')
+print(filenames)
+
+# Write your answer below
